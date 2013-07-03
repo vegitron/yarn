8602a3d4f901c579a5da0076b0b1157546fd64d6
Patrick Michaud <pmichaud@uw.edu>
2013-07-02 23:02:30 -0700

    Adding a deleted field to the thread

--- yarn/models.py
+++ yarn/models.py
@@ -10,6 +10,7 @@ class Thread(models.Model):
     name = models.CharField(max_length=100, unique=True, db_column='name')
     description = models.CharField(max_length=1000, db_column='description')
     is_private = models.NullBooleanField(db_column='is_private', null=True)
+    is_deleted = models.NullBooleanField(db_column='is_deleted', null=True)
     has_groups = models.BooleanField(db_column='has_groups')
 
     def person_has_access(self, person):
