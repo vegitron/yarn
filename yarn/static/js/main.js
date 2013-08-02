ec3fe0bfa68a5f877002905eec8d1089875a7a09
Charlon Palacay <charlon@gmail.com>
2013-08-02 13:41:34 -0700

    Undo the url bar hiding stuff.

--- yarn/static/js/main.js
+++ yarn/static/js/main.js
@@ -2,41 +2,6 @@ var sh = $(window).height(),
 	hh = $('.yarn-header').height(),
 	ch = sh - hh;
 	
-/*! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License */
-(function( win ){
-	var doc = win.document;
-
-	// If there's a hash, or addEventListener is undefined, stop here
-	if( !location.hash && win.addEventListener ){
-
-		//scroll to 1
-		win.scrollTo( 0, 1 );
-		var scrollTop = 1,
-			getScrollTop = function(){
-				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-			},
-
-			//reset to 0 on bodyready, if needed
-			bodycheck = setInterval(function(){
-				if( doc.body ){
-					clearInterval( bodycheck );
-					scrollTop = getScrollTop();
-					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-				}	
-			}, 15 );
-
-		win.addEventListener( "load", function(){
-			setTimeout(function(){
-				//at load, if user hasn't scrolled more than 20 or so...
-				if( getScrollTop() < 20 ){
-					//reset to hide addr bar at onload
-					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-				}
-			}, 0);
-		}, false );
-	}
-})( this );
-
 $(function() {
     
     $('#current_thread').on('click', function(e) {
@@ -77,6 +42,7 @@ $(window).resize(function() {
 
 function setContentHeights() {
     
+    $('.viewport').height(sh);
     $('.yarn-content').height(ch);
     $('.yarn-content-module').height(ch);
 }
