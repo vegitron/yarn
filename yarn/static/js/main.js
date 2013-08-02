bd1b7005e8624fb21d8daaf23ebe303b08301747
Charlon Palacay <charlon@gmail.com>
2013-08-02 10:11:25 -0700

    Initial slide transition of display panels.

--- yarn/static/js/main.js
+++ yarn/static/js/main.js
@@ -1,3 +1,7 @@
+var sh = $(window).height(),
+	hh = $('.yarn-header').height(),
+	ch = sh - hh;
+
 $(function() {
     
     $('#current_thread').on('click', function(e) {
@@ -14,5 +18,29 @@ $(function() {
     $('#myTab a').click(function (e) {
       e.preventDefault();
       $(this).tab('show');
-    })    
+    });
+    
+    
+    // set the container heights
+    setContentHeights();
+    
+    // slide the available thread list panel up
+    $('#available_thread_list').addClass('slide-up');
+    
+        
 });
+
+$(window).resize(function() {
+    
+    sh = $(window).height();
+	hh = $('.yarn-header').height();
+	ch = sh - hh;
+	
+    setContentHeights();
+    
+});
+
+function setContentHeights() {
+    $('.yarn-content').height(ch);
+    $('.yarn-content-module').height(ch);
+}
