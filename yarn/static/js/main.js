266dbb11e0af59517408edcd4b7c6fbc6b93a9fe
Patrick Michaud <pmichaud@uw.edu>
2013-11-07 13:23:27 -0800

    Revert "Removing the debouncing.  Makes the resize code use the id for elements, and adding an id to the viewport."
    This reverts commit 0cb14c652ed660a93eebfaf190c81da456492816.

--- yarn/static/js/main.js
+++ yarn/static/js/main.js
@@ -1,32 +1,69 @@
-$(function() {
+var sh = $(window).height(),
+	hh = $('.yarn-header').height(),
+	ch = sh - hh;
 
+var deBouncer = function($,cf,of, interval){
+    // deBouncer by hnldesign.nl
+    // based on code by Paul Irish and the original debouncing function from John Hann
+    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+    var debounce = function (func, threshold, execAsap) {
+        var timeout;
+        return function debounced () {
+            var obj = this, args = arguments;
+            function delayed () {
+                if (!execAsap)
+                    func.apply(obj, args);
+                timeout = null;
+            }
+            if (timeout)
+                clearTimeout(timeout);
+            else if (execAsap)
+                func.apply(obj, args);
+            timeout = setTimeout(delayed, threshold || interval);
+        };
+    };
+    jQuery.fn[cf] = function(fn){  return fn ? this.bind(of, debounce(fn)) : this.trigger(cf); };
+};
+
+// register debouncing functions
+deBouncer(jQuery,'smartresize', 'resize', 100);
+deBouncer(jQuery,'smartscroll', 'scroll', 100);
+deBouncer(jQuery,'smartmousemove', 'mousemove', 100);
+	
+$(function() {
+    
     $('#myTab a').click(function (e) {
         //e.preventDefault();
         $(this).tab('show');
     });
-
+    
     // set the container heights
     setContentHeights();
-
+    
     // slide the available thread list panel up
     $('#available_thread_list').addClass('slide-up');
-
+    
     // TODO: (mobile only) if user has no last active thread, show the sidebar 
     if (mobile) {
         $('body').addClass('offcanvas');
     }
-
-    $(window).on('resize', setContentHeights);
-
+    
 });
 
+// handle browser resizing (debounced)
+$(window).smartresize(function(e){
+    
+    sh = $(window).height();
+	hh = $('.yarn-header').height();
+	ch = sh - hh;
+	
+    setContentHeights();
+    
+});
 
 function setContentHeights() {
-    var window_height = $(window).height(),
-        header_height = $('.yarn-header').height(),
-        content_height = window_height - header_height;
-
-    $('#viewport').height(window_height);
-    $('#tabs').height(content_height);
-    $('#available_thread_list').height(content_height);
+    
+    $('.viewport').height(sh);
+    $('.yarn-content').height(ch);
+    $('.yarn-content-module').height(ch);
 }
